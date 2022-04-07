1064bfcb4fbe4e4fc3b5c08e0b73823900275156
Rubens Oliveira <39066466+sneburdf@users.noreply.github.com>
2022-04-07 15:26:49 -0300

    Upload inicial

---
+++ zeldaminiclone/src/zeldaminiclone/Game.java
@@ -0,0 +1,42 @@
+package zeldaminiclone;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static int WIDTH = 480, HEIGHT = 480;
+	
+
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		frame.add(game);
+		frame.setTitle("Mini Zelda");
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
